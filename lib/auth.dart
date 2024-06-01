bf06c8f680962f7bc1c6fb55307bb7cc148e8326
ushan-Tharuka <ushantharuka2222@gmail.com>
2024-06-02 01:02:23 +0530

    Add new code and update

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,15 +1,26 @@
+import 'package:firebase/models/UserModel.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthServices {
 //firebase instance
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  //create a user from firebase user with uid
+  UserModel? _userWithFirebaseUserUid(User? user) {
+    return user != null ? UserModel(uid: user.uid) : null;
+  }
+
+  //create the stream for checking the auth changes in the user
+  Stream<UserModel?> get user {
+    return _auth.authStateChanges().map(_userWithFirebaseUserUid);
+  }
+
   //sign in anonymous
   Future signInAnonymously() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
+      return _userWithFirebaseUserUid(user);
     } catch (err) {
       print(err.toString());
       return null;
